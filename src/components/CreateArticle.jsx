5ea4d77ae99eb6183e55388888f4cccb421bc880
Fraseer <fraseer1430@gmail.com>
2021-10-21 10:25:03 +0200

    forEach loop for category names

--- src/components/CreateArticle.jsx
+++ src/components/CreateArticle.jsx
@@ -1,21 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Header, Container, Form, Button } from "semantic-ui-react";
 import SemanticDatepicker from "react-semantic-ui-datepickers";
 import "react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css";
+import _ from "lodash";
 
 const CreateArticle = () => {
+  // eslint-disable-next-line no-unused-vars
   const [currentDate, setNewDate] = useState(null);
   const onChange = (event, data) => setNewDate(data.value);
 
-  const category_options = [
-    { key: 1, text: "World News", value: "worldNews" },
-    { key: 2, text: "Science & Enviroment", value: "scienceEnviroment" },
-    { key: 3, text: "Business", value: "business" },
-    { key: 4, text: "Tech", value: "tech" },
-    { key: 5, text: "Entertainment", value: "entertainment" },
-    { key: 6, text: "Culture & Health", value: "cultureHealth" },
-    { key: 7, text: "Sports", value: "sports" },
+  const options = [
+    "World News",
+    "Science & Enviroment",
+    "Business",
+    "Tech",
+    "Entertainment",
+    "Culture & Health",
+    "Sports",
   ];
+
+  useEffect(() => {
+    optionsLoop();
+  }, []);
+
+  let categoryOptions = [];
+  const optionsLoop = () => {
+    options.forEach((options) => {
+      categoryOptions.push({
+        text: options,
+        value: _.camelCase(options),
+      });
+    });
+  };
 
   return (
     <Container>
@@ -28,23 +44,18 @@ const CreateArticle = () => {
         Create Article
       </Header>
       <Form size="huge" data-cy="create-article">
-        <Form.Input  data-cy="title-input" placeholder="Title" />
-        <Form.Input
-          
-          data-cy="journalist-input"
-          placeholder="Journalists"
-        />
-        <Form.Input  data-cy="lede-input" placeholder="Lede" />
+        <Form.Input data-cy="title-input" placeholder="Title" />
+        <Form.Input data-cy="journalist-input" placeholder="Journalists" />
+        <Form.Input data-cy="lede-input" placeholder="Lede" />
         <Form.Group>
           <Form.Select
-            
             data-cy="category-input"
             placeholder="Category"
-            options={category_options}
+            options={categoryOptions}
           />
           <SemanticDatepicker onChange={onChange} />
         </Form.Group>
-        <Form.TextArea  data-cy="body-input" placeholder="Body" />
+        <Form.TextArea data-cy="body-input" placeholder="Body" />
         <Button data-cy="submit-button">Submit</Button>
       </Form>
     </Container>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 import "./styles/index.css";
 import "./styles/button.css";
 
-
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById("root")
 );
