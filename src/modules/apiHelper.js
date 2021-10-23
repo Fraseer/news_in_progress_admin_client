42ccb5df391f119c1eb6e4114cc8ce3ad4d042aa
Dorian Buck <dorianbuck@gmail.com>
2021-10-23 14:24:45 +0200

    adds author_ids in params

--- src/modules/apiHelper.js
+++ src/modules/apiHelper.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const Article = {
   async create(article) {
+    article["article"]["author_ids"] = []
     const params = article;
     const response = await axios.post("/api/articles/", params);
     return response;
