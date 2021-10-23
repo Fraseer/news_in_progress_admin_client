6320940d4fd88e6e87b3ee25eea19f066cd61654
Fraseer <fraseer1430@gmail.com>
2021-10-23 23:31:03 +0200

    Test for first line of table

--- cypress/integration/userCanSeeAListOfArticles.fixture.js
+++ cypress/integration/userCanSeeAListOfArticles.fixture.js
@@ -1,24 +1,33 @@
 describe("A list of articles displayed on the review page", () => {
   beforeEach(() => {
-    // cy.intercept("GET", "**api/articles/**", {
-    //   fixture: "indexResponseFromAPI.json",
-    //   statusCode: 200,
-    // });
+    cy.intercept("GET", "**api/articles/**", {
+      fixture: "indexResponseFromAPI.json",
+      statusCode: 200,
+    });
 
     cy.visit("/");
     cy.get("[data-cy=review-articles]").click();
   });
-
+  
   it("is expected to display a table with the following headers", () => {
     cy.get("[data-cy=review-table]").within(() => {
       cy.get("[data-cy=table-headers]").within(() => {
+        cy.get("[data-cy=id]").should("contain", "Article ID");
         cy.get("[data-cy=title]").should("contain", "Title");
         cy.get("[data-cy=journalist]").should("contain", "Journalist");
-        cy.get("[data-cy=lede]").should("contain", "Lede");
-        cy.get("[data-cy=category]").should("contain", "Category");
-        cy.get("[data-cy=body]").should("contain", "Body");
+        cy.get("[data-cy=created-at]").should("contain", "Created At");
         cy.get("[data-cy=published]").should("contain", "Published Status");
       });
+    });
+  });
+
+  it("is expected that the first article in the table will have", () => {
+    cy.get("[data-cy=row-1]").within(() => {
+      cy.get("[data-cy=id-1]").should("contain", 1);
+      cy.get("[data-cy=title-1]").should("contain", "Servicing and repairing electric cars requires new skills");
+      cy.get("[data-cy=journalist-1]").should("contain", "Journalist");
+      cy.get("[data-cy=created-at-1]").should("contain", "2021-10-05");
+      cy.get("[data-cy=published-1]").should("be.visible");
     });
   });
 });
--- src/components/ReviewArticles.jsx
+++ src/components/ReviewArticles.jsx
@@ -1,20 +1,32 @@
 import React from "react";
-import { Container, Table } from "semantic-ui-react";
+import { Container, Table, Checkbox } from "semantic-ui-react";
 
 const ReviewArticles = () => {
   return (
     <Container>
       <Table data-cy="review-table" compact celled>
         <Table.Header data-cy="table-headers">
+          <Table.HeaderCell data-cy="id">Article ID</Table.HeaderCell>
           <Table.HeaderCell data-cy="title">Title</Table.HeaderCell>
           <Table.HeaderCell data-cy="journalist">Journalist</Table.HeaderCell>
-          <Table.HeaderCell data-cy="lede">Lede</Table.HeaderCell>
-          <Table.HeaderCell data-cy="category">Category</Table.HeaderCell>
-          <Table.HeaderCell data-cy="body">Body</Table.HeaderCell>
+          <Table.HeaderCell data-cy="created-at">Created At</Table.HeaderCell>
           <Table.HeaderCell data-cy="published">
             Published Status
           </Table.HeaderCell>
         </Table.Header>
+        <Table.Body>
+          <Table.Row data-cy="row-1">
+            <Table.Cell data-cy="id-1">1</Table.Cell>
+            <Table.Cell data-cy="title-1">
+              Servicing and repairing electric cars requires new skills
+            </Table.Cell>
+            <Table.Cell data-cy="journalist-1">Bob Journalist</Table.Cell>
+            <Table.Cell data-cy="created-at-1">2021-10-05</Table.Cell>
+            <Table.Cell data-cy="published-1">
+              <Checkbox slider />
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
       </Table>
     </Container>
   );
